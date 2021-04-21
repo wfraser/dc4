0413505d41828452877655e6af5e1bf4dcc07ee9
William R. Fraser <wfraser@codewise.org>
2021-04-20 17:09:12 -0700

    simplify sum-numbers example error handling

--- examples/sum-numbers.rs
+++ examples/sum-numbers.rs
@@ -13,77 +13,7 @@
 
 use dc4::DC4;
 use dc4::parser::Action;
-use std::io::{self, Bytes, Read, Write};
-
-struct Input<R: Read> {
-    inner: Bytes<R>,
-    buf: Vec<u8>,
-    decimal: bool,
-}
-
-impl<R: Read> Input<R> {
-    pub fn new(reader: R) -> Self {
-        Self {
-            inner: reader.bytes(),
-            buf: vec![],
-            decimal: false,
-        }
-    }
-}
-
-impl<R: Read> Iterator for Input<R> {
-    type Item = io::Result<Vec<u8>>;
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let c = match self.inner.next() {
-                Some(Ok(c)) => c,
-                Some(Err(e)) => {
-                    return Some(Err(e));
-                }
-                None => {
-                    if self.buf.is_empty() {
-                        return None;
-                    } else {
-                        self.decimal = false;
-                        return Some(Ok(self.buf.split_off(0)));
-                    }
-                }
-            };
-
-            if (c as char).is_whitespace() {
-                if !self.buf.is_empty() {
-                    self.decimal = false;
-                    return Some(Ok(self.buf.split_off(0)));
-                }
-            } else if c == b'.' {
-                if self.decimal {
-                    return Some(Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        format!("invalid number in input: \"{}{}\"",
-                            String::from_utf8_lossy(&self.buf), c as char))));
-                } else {
-                    self.decimal = true;
-                    self.buf.push(c);
-                }
-            } else if c == b'-' {
-                if self.buf.is_empty() {
-                    self.buf.push(c);
-                } else {
-                    return Some(Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        format!("invalid number in input: \"{}{}\"",
-                            String::from_utf8_lossy(&self.buf), c as char))));
-                }
-            } else if (b'0' ..= b'9').contains(&c) {
-                self.buf.push(c);
-            } else {
-                return Some(Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    format!("invalid character in input: {:?}", c as char))));
-            }
-        }
-    }
-}
+use std::io::{self, BufRead, Write};
 
 struct Options {
     iradix: u32,
@@ -136,15 +66,16 @@ impl Options {
 // Thin wrapper around DC4::action. We only expect DCResult::Continue, so turn any other result
 // into an Err so we can use the question mark operator.
 fn action(dc: &mut DC4, action: Action, w: &mut impl Write)
-    -> Result<(), Result<dc4::DCResult, dc4::DCError>>
+    -> Result<(), dc4::DCError>
 {
     match dc.action(action, w) {
         Ok(dc4::DCResult::Continue) => Ok(()),
-        other => Err(other),
+        Ok(other) => Err(format!("unexpected result: {:?}", other).into()),
+        Err(other) => Err(other),
     }
 }
 
-fn run(r: impl Read, mut w: impl Write) -> Result<(), Result<dc4::DCResult, dc4::DCError>> {
+fn run(r: impl BufRead, mut w: impl Write) -> Result<(), dc4::DCError> {
     let mut dc = DC4::new("sum-numbers".to_owned());
 
     let opts = Options::parse(std::env::args())
@@ -154,33 +85,26 @@ fn run(r: impl Read, mut w: impl Write) -> Result<(), Result<dc4::DCResult, dc4:
         });
 
     if opts.oradix != 10 {
-        dc.push_number(&opts.oradix.to_string().into_bytes());
+        dc.push_number(&opts.oradix.to_string().into_bytes())?;
         action(&mut dc, Action::SetOutputRadix, &mut w)?;
     }
     if opts.iradix != 10 {
-        dc.push_number(&opts.iradix.to_string().into_bytes());
+        dc.push_number(&opts.iradix.to_string().into_bytes())?;
         action(&mut dc, Action::SetInputRadix, &mut w)?;
     }
 
     // initial value
-    dc.push_number("0");
+    dc.push_number("0").unwrap();
 
-    for result in Input::new(r) {
-        match result {
-            Ok(mut s) => {
-                if s.starts_with(b"-") {
-                    // dc uses '_' to designate negative numbers because '-' is used for
-                    // subtraction, so replace it.
-                    s[0] = b'_';
-                }
-                dc.push_number(&s);
-                action(&mut dc, Action::Add, &mut w)?;
-            }
-            Err(e) => {
-                eprintln!("ERROR: {}", e);
-                std::process::exit(2);
-            }
+    //for result in Input::new(r) {
+    for result in r.lines() {
+        let s = result.map_err(|e| dc4::DCError::from(format!("I/O error: {}", e)))?;
+        // dc uses '_' to designate negative numbers because '-' is used for subtraction, so
+        // replace it.
+        if let Err(e) = dc.push_number(s.replace('-', "_").trim()) {
+            return Err(format!("invalid input {:?}: {}", s, e).into());
         }
+        action(&mut dc, Action::Add, &mut w)?;
     }
 
     action(&mut dc, Action::PrintStack, &mut w)?;
@@ -193,6 +117,7 @@ fn main() {
     let stdin_lock = stdin.lock();
 
     if let Err(result) = run(stdin_lock, w) {
-        panic!("unexpected result: {:?}", result);
+        eprintln!("error: {}", result);
+        std::process::exit(2);
     }
 }
